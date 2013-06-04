5190d7676a2f33b2e07377a9a938da3560014812
Alex Vandiver <alexmv@bestpractical.com>
2013-06-04 17:59:29 -0400

    Add documentation on existing configuration options

--- etc/Assets_Config.pm
+++ etc/Assets_Config.pm
@@ -1,3 +1,28 @@
+=head1 NAME
+
+Assets configuration
+
+=head1 Base configuration
+
+=over 4
+
+=item C<Lifecycles>
+
+Each asset catalog uses a lifecycle, exactly as queues for tickets do --
+and the configuration is the same.  See L<RT_Config/Lifecycles> for
+documentation on them.  Asset lifecycles are differentiated from dicket
+lifecycles by their type:
+
+    assets => {
+        type     => "asset",     # Note the type, here
+        initial  => [ 'new' ],
+        active   => [ 'allocated', 'in-use' ],
+        inactive => [ 'recycled', 'stolen', 'deleted' ],
+        # ...
+    }
+
+=cut
+
 Set(%Lifecycles,
     assets => {
         type     => "asset",
@@ -28,6 +53,51 @@ Set(%Lifecycles,
     },
 );
 
+=item C<@AssetQueues>
+
+This should be a list of names of queues whose tickets should always
+display the "Assets" box.  This is useful for queues which deal
+primarily with assets, as it provides a ready box to link an asset to
+the ticket, even when the ticket has no related assets yet.
+
+=cut
+
+Set(@AssetQueues, ());
+
+=item C<$DefaultCatalog>
+
+This provides the default catalog after a user initially logs in.
+However, the default catalog is "sticky," and so will remember the
+last-selected catalog thereafter.
+
+=cut
+
+Set($DefaultCatalog, 'General assets');
+
+=item C<$AssetSearchFormat>
+
+The format that results of the asset search are displayed with.
+
+=cut
+
+Set($AssetSearchFormat, q[
+    '<a href="__WebHomePath__/Asset/Display.html?id=__id__">__Name__</a>/TITLE:Name',
+    Description,
+    '__Status__ (__Catalog__)/TITLE:Status',
+    OwnerName,
+    HeldBy,
+    Contacts,
+    '__ActiveTickets__ __InactiveTickets__/TITLE:Related tickets',
+]);
+
+=item C<$AssetSummaryFormat>
+
+The information that is displayed on ticket display pages about assets
+related to the ticket.  This is displayed in a table beneath the asset
+name.
+
+=cut
+
 Set($AssetSummaryFormat, q[
     '<a href="__WebHomePath__/Asset/Display.html?id=__id__">__Name__</a>/TITLE:Name',
     Description,
@@ -38,6 +108,14 @@ Set($AssetSummaryFormat, q[
     '__ActiveTickets__ __InactiveTickets__/TITLE:Related tickets',
 ]);
 
+=item C<$AssetSummaryRelatedTicketsFormat>
+
+The information that is displayed on ticket display pages about tickets
+related to assets related to the ticket.  This is displayed as a list of
+tickets underneath the asset properties.
+
+=cut
+
 Set($AssetSummaryRelatedTicketsFormat, q[
     '<a href="__WebPath__/Ticket/Display.html?id=__id__">__id__</a>',
     '(__OwnerName__)',
@@ -46,11 +124,23 @@ Set($AssetSummaryRelatedTicketsFormat, q[
     Status,
 ]);
 
+=item C<%AdminSearchResultFormat>
+
+The C<Catalogs> key of this standard RT configuration option (see
+L<RT_Config/%AdminSearchResultFormat>) controls how catalogs are
+displayed in their list in the admin pages.
+
+=cut
+
 Set(%AdminSearchResultFormat,
     Catalogs =>
         q{'<a href="__WebPath__/Admin/Assets/Catalogs/Modify.html?id=__id__">__id__</a>/TITLE:#'}
         .q{,'<a href="__WebPath__/Admin/Assets/Catalogs/Modify.html?id=__id__">__Name__</a>/TITLE:Name'}
         .q{,__Description__,__Lifecycle__,__Disabled__},
 );
+
+=back
+
+=cut
 
 1;
