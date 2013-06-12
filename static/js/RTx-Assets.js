898682764b6639785be8a1ae5c32a339366c1c7d
sunnavy <sunnavy@bestpractical.com>
2013-06-12 21:23:29 +0800

    data-autocomplete is in core now

--- static/js/RTx-Assets.js
+++ static/js/RTx-Assets.js
@@ -6,20 +6,6 @@ jQuery(function() {
             .modal();
     };
 
-    jQuery("input[data-autocomplete]").each(function(){
-        var input = jQuery(this);
-        var what  = input.attr("data-autocomplete");
-        var wants = input.attr("data-autocomplete-return");
-
-        if (!what || !what.match(/^(Users|Groups)$/))
-            return;
-
-        input.autocomplete({
-            source: RT.Config.WebPath + "/Helpers/Autocomplete/" + what
-                    + (wants ? "?return=" + wants : "")
-        });
-    });
-
     var assets = jQuery("#assets-accordion");
     assets.accordion({
         // Open the accordion if there's only one fold, otherwise start with
