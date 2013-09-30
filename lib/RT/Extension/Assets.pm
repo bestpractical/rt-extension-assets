b2ba810c507ceb8eb9ee3f31c5eceef7ee421a5e
Alex Vandiver <alexmv@bestpractical.com>
2013-09-30 14:37:44 -0400

    Bump the version slightly; still pre-1.0

--- lib/RT/Extension/Assets.pm
+++ lib/RT/Extension/Assets.pm
@@ -50,7 +50,7 @@ use strict;
 use warnings;
 package RT::Extension::Assets;
 
-our $VERSION = '0.01';
+our $VERSION = '0.90';
 
 # Loaded so they're available and rights are injected.
 use RT::Catalog;
