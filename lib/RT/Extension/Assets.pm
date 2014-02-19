bfea1adb60a29b005b1635264c706194d65d26e1
Alex Vandiver <alexmv@bestpractical.com>
2014-02-19 15:10:10 -0500

    Releng for 1.0rc2

--- lib/RT/Extension/Assets.pm
+++ lib/RT/Extension/Assets.pm
@@ -50,7 +50,7 @@ use strict;
 use warnings;
 package RT::Extension::Assets;
 
-our $VERSION = '1.0rc1';
+our $VERSION = '1.0rc2';
 
 # Loaded so they're available and rights are injected.
 use RT::Catalog;
