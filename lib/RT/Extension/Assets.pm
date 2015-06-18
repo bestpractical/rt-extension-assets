9b1256aa57d7b6216b8e64b76d18b98ac558f05d
sunnavy <sunnavy@bestpractical.com>
2015-06-18 23:27:37 +0800

    first version of asset query builder
    this is mostly a clone from ticket's query builder.
    in 4.4 when asset is in core, we shall refactor it to reduce code duplication.

--- lib/RT/Extension/Assets.pm
+++ lib/RT/Extension/Assets.pm
@@ -412,6 +412,93 @@ RT->AddJavaScript("RTx-Assets.js");
     }
 }
 
+{
+
+    package RT::Interface::Web::QueryBuilder::Tree;
+    sub GetReferencedCatalogs {
+        my $self = shift;
+
+        my $catalogs = {};
+
+        $self->traverse(
+            sub {
+                my $node = shift;
+
+                return if $node->isRoot;
+                return unless $node->isLeaf;
+
+                my $clause = $node->getNodeValue();
+                return unless $clause->{ Key } eq 'Catalog';
+                return unless $clause->{ Op } eq '=';
+
+                $catalogs->{ $clause->{ RawValue } } = 1;
+            }
+        );
+
+        return $catalogs;
+    }
+
+    sub ParseAssetSQL {
+        my $self = shift;
+        my %args = (
+            Query       => '',
+            CurrentUser => '',    #XXX: Hack
+            @_
+        );
+        my $string = $args{ 'Query' };
+
+        my @results;
+
+        my %field = %{ RT::Assets->new( $args{ 'CurrentUser' } )->FIELDS };
+        my %lcfield = map { ( lc( $_ ) => $_ ) } keys %field;
+
+        my $node = $self;
+
+        my %callback;
+        $callback{ 'OpenParen' } = sub {
+            $node = __PACKAGE__->new( 'AND', $node );
+        };
+        $callback{ 'CloseParen' } = sub { $node = $node->getParent };
+        $callback{ 'EntryAggregator' } = sub { $node->setNodeValue( $_[ 0 ] ) };
+        $callback{ 'Condition' } = sub {
+            my ( $key, $op, $value ) = @_;
+            my $rawvalue = $value;
+
+            my ( $main_key ) = split /[.]/, $key;
+
+            my $class;
+            if ( exists $lcfield{ lc $main_key } ) {
+                $key =~ s/^[^.]+/ $lcfield{ lc $main_key } /e;
+                ( $main_key ) = split /[.]/, $key;    # make the case right
+                $class = $field{ $main_key }->[ 0 ];
+            }
+            unless ( $class ) {
+                push @results, [ $args{ 'CurrentUser' }->loc( "Unknown field: [_1]", $key ), -1 ];
+            }
+
+            if ( lc $op eq 'is' || lc $op eq 'is not' ) {
+                $value = 'NULL';                      # just fix possible mistakes here
+            }
+            elsif ( $value !~ /^[+-]?[0-9]+$/ ) {
+                $value =~ s/(['\\])/\\$1/g;
+                $value = "'$value'";
+            }
+
+            if ( $key =~ s/(['\\])/\\$1/g or $key =~ /[^{}\w\.]/ ) {
+                $key = "'$key'";
+            }
+
+            my $clause = { Key => $key, Op => $op, Value => $value, RawValue => $rawvalue };
+            $node->addChild( __PACKAGE__->new( $clause ) );
+        };
+        $callback{ 'Error' } = sub { push @results, @_ };
+
+        require RT::SQL;
+        RT::SQL::Parse( $string, \%callback );
+        return @results;
+    }
+}
+
 =head1 INSTALLATION
 
 Assets requires version 4.2.1 or higher of RT.
@@ -427,6 +514,8 @@ Assets requires version 4.2.1 or higher of RT.
 This step may require root permissions.
 
 =item Patch your RT
+
+    patch -d /opt/rt4 -p1 < patches/assets-query-builder.patch
 
 Assets requires a small patch to work on versions of RT prior to 4.2.3.
 To patch RT, run:
